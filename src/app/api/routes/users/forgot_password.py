e80240389ef68e5b5c7d66d39c39166807167036
dolbikcop <polyabikka@gmail.com>
2024-01-14 21:19:37 +0500

    update templates

--- src/app/api/routes/users/forgot_password.py
+++ src/app/api/routes/users/forgot_password.py
@@ -1,15 +1,14 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Body, Depends, HTTPException, Form
+from fastapi import APIRouter, Body, Depends, Form
 from fastapi_users import exceptions
-from fastapi_users.router import ErrorCode
 from fastapi_users.router.reset import RESET_PASSWORD_RESPONSES
 from pydantic import EmailStr
 from starlette import status
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
 
-from app.modules.users.utils.forgot_password import get_reset_password_template
+from app.modules.users.utils.forgot_password import get_reset_password_template, get_result_password_template
 from app.modules.users.manager import get_user_manager
 
 password_router = APIRouter()
@@ -42,27 +41,17 @@ async def reset_password(
     user_manager=Depends(get_user_manager),
 ):
     try:
-        await user_manager.reset_password(token, password, request)
-        return HTTPException(status_code=200, detail={"Успешно изменен пароль."})
+        user = await user_manager.reset_password(token, password, request)
+        return HTMLResponse(content=get_result_password_template(True), status_code=200)
     except (
         exceptions.InvalidResetPasswordToken,
         exceptions.UserNotExists,
         exceptions.UserInactive,
+        exceptions.InvalidPasswordException
     ):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=ErrorCode.RESET_PASSWORD_BAD_TOKEN,
-        )
-    except exceptions.InvalidPasswordException as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail={
-                "code": ErrorCode.RESET_PASSWORD_INVALID_PASSWORD,
-                "reason": e.reason,
-            },
-        )
+        return HTMLResponse(content=get_result_password_template(False), status_code=400)
 
 
-@password_router.post("/verify_reset_password")
-async def reset_password(token: Annotated[str, Form()]):
+@password_router.get("/verify_reset_password")
+async def get_reset_password_form(token: str):
     return HTMLResponse(content=get_reset_password_template(token), status_code=200)
--- src/app/api/routes/users/verify_user.py
+++ src/app/api/routes/users/verify_user.py
@@ -1,12 +1,13 @@
-from fastapi import Body, Depends, APIRouter, HTTPException
+from fastapi import Body, Depends, APIRouter
 from fastapi_users import exceptions
-from fastapi_users.router import ErrorCode
 from pydantic import EmailStr
 from starlette import status
 from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
 from app.modules.users.manager import get_user_manager
 from app.modules.users.schemas import UserRead
+from app.modules.users.utils.confirm_email import get_result_template
 
 verify_router = APIRouter()
 
@@ -28,22 +29,16 @@ async def request_verify_token(
     return None
 
 
-@verify_router.get("/verify", response_model=UserRead, tags=["verify-back"])
+@verify_router.get("/verify", tags=["verify-back"])
 async def verify(
     token: str,
     request: Request,
     user_manager=Depends(get_user_manager),
 ):
     try:
-        user = await user_manager.verify(token, request)
-        return user
+        await user_manager.verify(token, request)
+        return HTMLResponse(content=get_result_template(True), status_code=200)
     except (exceptions.InvalidVerifyToken, exceptions.UserNotExists):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=ErrorCode.VERIFY_USER_BAD_TOKEN,
-        )
+        return HTMLResponse(content=get_result_template(False), status_code=400)
     except exceptions.UserAlreadyVerified:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=ErrorCode.VERIFY_USER_ALREADY_VERIFIED,
-        )
+        return HTMLResponse(content=get_result_template(True), status_code=200)
